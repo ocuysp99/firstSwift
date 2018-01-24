30fc00cf0a2d5c145bfc1acf28e0e6a43646f69a
Your Name <you@example.com>
2018-01-24 09:53:51 +0800

    ＯＫ

---
+++ pj01/recordViewController.swift
@@ -0,0 +1,41 @@
+//
+//  recordViewController.swift
+//  pj01
+//
+//  Created by Kevin on 2018/1/23.
+//  Copyright © 2018年 Kevin. All rights reserved.
+//
+
+import UIKit
+
+class recordViewController: UIViewController {
+
+    @IBOutlet weak var txtMoney: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnSave(_ sender: Any) {
+        print("金額：\(txtMoney.text!)")
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
